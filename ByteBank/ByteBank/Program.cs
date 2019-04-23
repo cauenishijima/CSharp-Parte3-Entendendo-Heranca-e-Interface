72316a6ce95cc951aebe0a09a66c99e50675bffe
Cauê Nishijima <caue_nishijima@yahoo.com.br>
2019-04-22 21:12:09 -0300

    Inicio do projeto ByteBank
    Criação da classe Funcionario no namespace ByteBank.Funcionarios

---
+++ ByteBank/ByteBank/Program.cs
@@ -0,0 +1,25 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+            funcionario.Nome = "Cauê Nishijma";
+            funcionario.CPF = "229.984.398-10";
+            funcionario.Salario = 5000;
+
+            Console.WriteLine(funcionario.Nome);
+            Console.WriteLine(funcionario.GetBonificacao());
+
+            Console.ReadLine();
+        }
+    }
+}
